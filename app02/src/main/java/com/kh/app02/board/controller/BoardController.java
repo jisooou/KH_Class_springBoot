02e0ac519e28da19d05e217319b0977a6b1531d7
DESKTOP-38924LQ\user1 <jisoou4@gmail.com>
2024-06-13 17:38:15 +0900

    app02 수정사항 커밋

--- app02/src/main/java/com/kh/app02/board/controller/BoardController.java
+++ app02/src/main/java/com/kh/app02/board/controller/BoardController.java
@@ -1,12 +1,17 @@
 package com.kh.app02.board.controller;
 
+import com.google.gson.Gson;
 import com.kh.app02.board.service.BoardService;
 import com.kh.app02.board.vo.BoardVo;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @Controller
@@ -16,13 +21,15 @@ public class BoardController {
     private BoardService service;
 
     @GetMapping("board/list")
-    public void getBoardList( ){
-
+    @ResponseBody
+    public List<BoardVo> getBoardList() throws IOException {
 //        service
         List<BoardVo> voList = service.getBoardList();
         for (BoardVo boardVo : voList) {
             System.out.println("boardVo = " + boardVo);
         }
+
+        return voList;
     }
 
     @PostMapping ("board/write")
