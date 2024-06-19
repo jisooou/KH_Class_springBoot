d42a99da2f64a710b8a20a0d2ac9a8c36968ef08
DESKTOP-38924LQ\user1 <jisoou4@gmail.com>
2024-06-19 14:47:22 +0900

    app04 변경 후 커밋

--- app04/src/main/java/com/kh/app04/board/vo/BoardVo.java
+++ app04/src/main/java/com/kh/app04/board/vo/BoardVo.java
@@ -1,6 +1,9 @@
 package com.kh.app04.board.vo;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Data
 public class BoardVo {
@@ -8,5 +11,6 @@ public class BoardVo {
     private String no;
     private String title;
     private String content;
+    private List<MultipartFile> attachmentList;
 
 }
