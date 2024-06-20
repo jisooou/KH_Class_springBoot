e12d61548796a5b4f9e5e46f3f490801f84c5acf
DESKTOP-38924LQ\user1 <jisoou4@gmail.com>
2024-06-20 17:50:17 +0900

    app04 커밋

--- app04/src/main/java/com/kh/app04/home/controller/HomeController.java
+++ app04/src/main/java/com/kh/app04/home/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.kh.app04.home.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("home")
 public class HomeController {
 
+    @Value("${atk}")
+    private int atk;
+
+    @Value("${def}")
+    private int def;
+
+    @Value("${email}")
+    private String email;
+
+    @Value("#{1+1}")
+    private int result;
+
 //    홈페이지 보여주기
     @GetMapping
     public String m01(){
+        System.out.println("result = " + result);
         return "home";
     }
 
