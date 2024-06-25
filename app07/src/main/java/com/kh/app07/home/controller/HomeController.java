e60af0240b3f6eafc3cb9327751606de84e43dcf
DESKTOP-38924LQ\user1 <jisoou4@gmail.com>
2024-06-25 13:52:37 +0900

    app07 커밋

--- app07/src/main/java/com/kh/app07/home/controller/HomeController.java
+++ app07/src/main/java/com/kh/app07/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.kh.app07.home.controller;
 
+import com.kh.app07.annotation.HelloAno;
+import com.kh.app07.annotation.KhAno;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("home")
+@KhAno
 public class HomeController {
 
     @GetMapping
