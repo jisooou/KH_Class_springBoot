9856dbf69fe393aa4aa9091bb96ed4f35c09846a
DESKTOP-38924LQ\user1 <jisoou4@gmail.com>
2024-06-28 18:05:10 +0900

    app99 커밋

---
+++ app99/src/main/java/com/kh/app99/member/vo/MemberVo.java
@@ -0,0 +1,15 @@
+package com.kh.app99.member.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVo {
+
+    private String no;
+    private String id;
+    private String pwd;
+    private String nick;
+    private String del_yn;
+    private String enroll_date;
+
+}
